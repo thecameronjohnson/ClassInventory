e156e5df9c88e09f0fabd0d7c0c53345fe2353fb
cam johnson <the.camjohnson@gmail.com>
2019-02-19 09:59:09 -0500

    added player adding

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -13,6 +13,7 @@ namespace ClassInventory
     public partial class Form1 : Form
     {
         // TODO - create a List to store all inventory objects
+        List<Player> playerList = new List<Player>();
 
         public Form1()
         {
@@ -28,7 +29,21 @@ namespace ClassInventory
             // TODO - add object to global list
 
             // TODO - display message to indicate addition made
-        }
+
+            string name, team, position;
+            int age;
+
+            name = nameInput.Text;
+            team = teamInput.Text;
+            position = positionInput.Text;
+            age = Convert.ToInt32(ageInput.Text);
+
+
+            Player c = new Player(name, team, position, age);
+            playerList.Add(c);
+
+            errorOutput.Text = "Player added";
+            }
 
         private void removeButton_Click(object sender, EventArgs e)
         {
